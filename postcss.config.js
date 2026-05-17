99350842dd15f7bafb689827f51ac4db0bf4c421
Denis (Ideator) <vtndenis@gmail.com>
2026-05-17 09:15:56 +0000

    Fix postcss config and add dev dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
-/**
- * vscode-tailwindcss does not use this file — kept for reference.
- * Tailwind config is in tailwind.config.js
- */
-
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
